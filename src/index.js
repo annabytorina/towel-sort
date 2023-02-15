807bf2c2716c5aa62b26f067fadefcce5c1e096f
Ania <hannabutorina@gmail.com>
2023-02-15 15:18:07 +0300

    fix: add undefined check

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (arr) {
-  if (arr === []) {
+  
+  if (arr === undefined || !arr.length) {
     return [];
   } else {
     for (let i=1;i<arr.length;i+=2){
       arr[i].reverse();
     }
     return arr.flat(); 
-  }
-  
+  } 
    
 }
